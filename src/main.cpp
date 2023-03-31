7960fa0c8015369c44004d81a31f74f65532d3ce
meequrox <117393211+meequrox@users.noreply.github.com>
2023-03-31 23:21:06 +0700

    Check if yt-dlp exists on startup

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,19 @@
 
 #include "args.hpp"
 
+#ifdef _WIN32
+    #define PIPE_TO_NULL "2>NUL 1>NUL"
+#else
+    #define PIPE_TO_NULL "2>/dev/null 1>/dev/null"
+#endif
+
 int main(int argc, char** argv) {
+    if (system("yt-dlp -h " PIPE_TO_NULL) != 0) {
+        std::cerr << "yt-dlp not found" << std::endl;
+        std::cerr << "Install: https://github.com/yt-dlp/yt-dlp/wiki/Installation" << std::endl;
+        return EXIT_FAILURE;
+    }
+
     mqr::args args;
     if (!args.parse(argc, argv)) return EXIT_FAILURE;
 
