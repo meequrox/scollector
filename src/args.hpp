970cbe76ef4904da5111be90c496b528276fa175
meequrox <117393211+meequrox@users.noreply.github.com>
2023-03-31 22:49:24 +0700

    args: add -o option to set output directory

--- src/args.hpp
+++ src/args.hpp
@@ -1,17 +1,26 @@
 #ifndef ARGS_HPP
 #define ARGS_HPP
 
+#include <filesystem>
 #include <iomanip>
 #include <iostream>
 
 namespace mqr {
+namespace fs = std::filesystem;
+
+#define SET_LWIDTH(w) std::left << std::setw(w)
+
 class args {
    public:
     bool verbose;
+    fs::path output;
 
     args() { reset_options(); }
 
-    void reset_options() { verbose = false; }
+    void reset_options() {
+        verbose = false;
+        output = fs::current_path();
+    }
 
     bool parse(int argc, char** argv) {
         if (!argv) return false;
@@ -28,7 +37,30 @@ class args {
                 return false;
             }
 
-            if (arg == "-v" || arg == "--verbose") verbose = true;
+            if (arg == "-v")
+                verbose = true;
+            else if (arg == "-o") {
+                if (i + 1 >= argc) {
+                    help();
+                    return false;
+                }
+
+                std::string arg_next = argv[i + 1];
+                if (fs::exists(arg_next) && fs::is_directory(arg_next)) {
+                    output = arg_next;
+                    output = fs::absolute(output.lexically_normal());
+                } else {
+                    std::cerr << arg_next << " doesn't exist or isn't a directory" << std::endl
+                              << std::endl;
+                    help();
+                    return false;
+                }
+
+                i++;
+            } else {
+                help();
+                return false;
+            }
         }
 
         return true;
@@ -37,8 +69,12 @@ class args {
     void help() {
         std::cout << "scollector" << std::endl << std::endl;
         std::cout << "options:" << std::endl;
-        std::cout << std::left << std::setw(30) << "  -v, --verbose"
+        std::cout << SET_LWIDTH(16) << " -h,--help"
+                  << "print this help message" << std::endl;
+        std::cout << SET_LWIDTH(16) << " -v"
                   << "print extra info" << std::endl;
+        std::cout << SET_LWIDTH(16) << " -o"
+                  << "set output directory" << std::endl;
     }
 };
 }  // namespace mqr
--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,9 @@ int main(int argc, char** argv) {
     mqr::args args;
     if (!args.parse(argc, argv)) return EXIT_FAILURE;
 
+    if (args.verbose) {
+        std::cout << "Output directory: " << args.output << std::endl;
+    }
+
     return EXIT_SUCCESS;
 }
