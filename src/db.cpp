130395822eb463d912293dcd4c179a4d8f8edc7b
meequrox <117393211+meequrox@users.noreply.github.com>
2023-04-05 01:07:04 +0700

    db.cpp: print error message if it occurs

--- src/db.cpp
+++ src/db.cpp
@@ -12,6 +12,7 @@ bool db_open(const char* path, sqlite3** dbpp) {
 
     constexpr char sql_create[] = "CREATE TABLE songs(id INT NOT NULL UNIQUE,PRIMARY KEY(id));";
     sqlite3_exec(*dbpp, sql_create, nullptr, nullptr, nullptr);
+
     return true;
 }
 
@@ -19,20 +20,28 @@ void db_begin(sqlite3* dbp) {
     if (!dbp) return;
 
     std::string query = "BEGIN TRANSACTION;";
+    char* errmsg = nullptr;
 
-    if (sqlite3_exec(dbp, query.c_str(), nullptr, nullptr, nullptr) != SQLITE_OK) {
+    if (sqlite3_exec(dbp, query.c_str(), nullptr, nullptr, &errmsg) != SQLITE_OK) {
         std::cout << __FUNCTION__ << ": failed " << query << std::endl;
+        std::cout << __FUNCTION__ << ": " << errmsg << std::endl;
+
+        sqlite3_free(errmsg);
     }
 }
 
 void db_insert(sqlite3* dbp, const uint32_t id) {
     if (!dbp) return;
 
-    constexpr char sql_insert[] = "INSERT INTO songs(id) VALUES";
-    std::string query = std::string(sql_insert) + "(" + std::to_string(id) + ");";
+    std::string sql_insert = "INSERT INTO songs(id) VALUES(";
+    std::string query = sql_insert + std::to_string(id) + ");";
+    char* errmsg = nullptr;
 
-    if (sqlite3_exec(dbp, query.c_str(), nullptr, nullptr, nullptr) != SQLITE_OK) {
+    if (sqlite3_exec(dbp, query.c_str(), nullptr, nullptr, &errmsg) != SQLITE_OK) {
         std::cout << __FUNCTION__ << ": failed " << query << std::endl;
+        std::cout << __FUNCTION__ << ": " << errmsg << std::endl;
+
+        sqlite3_free(errmsg);
     }
 }
 
@@ -40,9 +49,13 @@ void db_end(sqlite3* dbp) {
     if (!dbp) return;
 
     std::string query = "END TRANSACTION;";
+    char* errmsg = nullptr;
 
-    if (sqlite3_exec(dbp, query.c_str(), nullptr, nullptr, nullptr) != SQLITE_OK) {
+    if (sqlite3_exec(dbp, query.c_str(), nullptr, nullptr, &errmsg) != SQLITE_OK) {
         std::cout << __FUNCTION__ << ": failed " << query << std::endl;
+        std::cout << __FUNCTION__ << ": " << errmsg << std::endl;
+
+        sqlite3_free(errmsg);
     }
 }
 
@@ -59,12 +72,16 @@ static int db_exists_callback(void* exists, int count, char** data, char** colum
 bool db_id_exists(const uint32_t id, sqlite3* dbp) {
     if (!dbp) return false;
 
-    constexpr char sql_exists[] = "SELECT 1 FROM songs WHERE id=";
-    std::string query = std::string(sql_exists) + std::to_string(id) + ";";
+    std::string sql_exists = "SELECT 1 FROM songs WHERE id=";
+    std::string query = sql_exists + std::to_string(id) + ";";
+    char* errmsg = nullptr;
 
     bool exists = false;
-    if (sqlite3_exec(dbp, query.c_str(), db_exists_callback, (void*)&exists, nullptr) != SQLITE_OK) {
+    if (sqlite3_exec(dbp, query.c_str(), db_exists_callback, (void*)&exists, &errmsg) != SQLITE_OK) {
         std::cout << __FUNCTION__ << ": failed " << query << std::endl;
+        std::cout << __FUNCTION__ << ": " << errmsg << std::endl;
+
+        sqlite3_free(errmsg);
     }
 
     return exists;
